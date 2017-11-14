b315cc0ac2281b45db319e64a839c2277f9cfdac
huangliang6 <huangliang6@lenovo.com>
2017-11-14 15:38:40 +0800

    add detail result.

--- order-service/src/main/java/com/yiguan/jigsaw/order/services/argument/OrderCreationResp.java
+++ order-service/src/main/java/com/yiguan/jigsaw/order/services/argument/OrderCreationResp.java
@@ -1,10 +1,33 @@
 package com.yiguan.jigsaw.order.services.argument;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
 public class OrderCreationResp {
-  private Long id;
+  private Integer uid;
+  private BigDecimal totalPrice;
+
+  private List<OrderCreationReq.OrderItem> items;
+
+  @Getter
+  @Setter
+  @Builder
+  @EqualsAndHashCode
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class OrderItem {
+    private long pid;
+    private String name;
+    private BigDecimal price;
+    private Integer amount;
+  }
 }
